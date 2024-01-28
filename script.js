1a8a5a2c417164b8a9576e446165fbb25a8453ca
Alyece Watson <153481120+Watsonaj0316@users.noreply.github.com>
2024-01-28 15:22:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
-// Assignment Code
 var characterLength = 128;
-var choiceArr =[];
+var choiceArr ='';
 
-var specialChatrArr =['!','@','#','$','%','^','&','*','(',')','_','-','=','+'];
-var loserCaseArr =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numberArr =['0','1','2','3','4','5','6','7','8','9'];
-
+const specialChatrArr =['!','@','#','$','%','^','&','*','(',')','_','-','=','+'].join('');
+const lowerCaseArr =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].join('');
+const upperCaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'].join('');
+const numberArr =['0','1','2','3','4','5','6','7','8','9'].join('');
+var passwordText = document.querySelector("#password");
 var generateBTN = document.querySelector("#generate");
 
 generateBTN.addEventListener("click", writePassword);
 
 function writePassword() { 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
+  generatePassword();
+};
 
 function generatePassword() { 
-  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
-  if (characterLength < 8 ||characterLength > 128 ){
+  characterLength = prompt("How many characters do you want your password to be? (8 - 128 characters");
+  if ( characterLength < 8 || characterLength > 128 ){
     alert("Character length has to be a number between 8 - 128 digits. Please try again.");
-  return false;
-  }
+  };
   if (confirm("Would you like to use lowercase letters in your password?")) {
-    choiceArr = choiceArr.concat(loserCaseArr);
-  }
-
+    choiceArr += lowerCaseArr;
+  };
   if (confirm("Would you like to use uppercase letters in your password?")) {
-    choiceArr = choiceArr.concat(upperCaseArr);
-  }
+    choiceArr += upperCaseArr;
+  };
 
   if (confirm("Would you like to use special letters in your password?")) {
-    choiceArr = choiceArr.concat(specialChatrArr);
-  }
+    choiceArr += specialChatrArr;
+  };
 
   if (confirm("Would you like to use numbers in your password?")) {
-    choiceArr = choiceArr.concat(numberArr);
-  
-  }
+    choiceArr += numberArr;
+  };
   
   //Build a loop as many times as the number as characters the user enters "charcterlength"
   
   // I would generatePassword based on the promnpts
   var password = "";
-  for (var i = 8; i < characterLength; i++); {
-    var randomIndex = Math.floor(Math.random() * choiceArr.length);
-    password = password + choiceArr[randomIndex];
+  for ( i = 0; i < characterLength; i++); {
+    var randomIndex = Math.floor(Math.random() * (choiceArr.length-1));
+    password += choiceArr[randomIndex];
     
   }
-  return password;
+  passwordText.value = password;
+  return;
   
 // For every loop pick a random character from the choice array
 
-}
-console.log(password);
+};
