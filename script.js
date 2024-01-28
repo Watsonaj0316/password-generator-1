909b50eddd377b6e2d2b31b0cbcc44fae18765da
Alyece Watson <153481120+Watsonaj0316@users.noreply.github.com>
2024-01-27 18:09:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
 // Assignment Code
-function generatePassword(){}
+var characterLength = 128;
+var choiceArr =[];
 
-const passwordLength = prompt("Enter a password length between 8 and 128");
-console.log(passwordLength);
-char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-num = "0123456789";
-sym = "!@$%^&*()-_+=";
+var specialChatrArr =['!','@','#','$','%','^','&','*','(',')','_','-','=','+'];
+var loserCaseArr =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr =['0','1','2','3','4','5','6','7','8','9'];
 
+var generateBTN = document.querySelector("#generate");
 
+generateBTN.addEventListener("click", writePassword);
 
-if (window.confirm("Contain a special character")) {
-  window.open("special character", "Thanks for selecting!");
-}
+function writePassword() { 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-if (window.confirm("Contain uppercase and lowercase characters")) {
-  window.open("upper and lowercase characters", "Thanks for selecting!");
-}
-
-if (window.confirm("Contain a numeric character")) {
-  window.open("numeric character", "Thanks for selecting!");
+  passwordText.value = password;
 
 }
 
-function generatePassword(length){
-const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$%^&*()-_+=';
-let password = '';
-for (let i = 0; i < length; i++) {
-const randomINDEX = Math.floor(Math.random() * charset.length);
-password += charset[randomINDEX];
-}
 
+
+function generatePassword() { 
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
+  if (characterLength < 8 ||characterLength > 128 ){
+    alert("Character length has to be a number between 8 - 128 digits. Please try again.");
+  return false;
+  }
+  if (confirm("Would you like to use lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(loserCaseArr);
+  }
+
+  if (confirm("Would you like to use uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+
+  if (confirm("Would you like to use special letters in your password?")) {
+    choiceArr = choiceArr.concat(specialChatrArr);
+  }
+
+  if (confirm("Would you like to use numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  
+  }
+  
+  //Build a loop as many times as the number as characters the user enters "charcterlength"
+  
+  // I would generatePassword based on the promnpts
+  var password = "";
+  for (var i = 8; i < characterLength; i++); {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+    
+  }
   return password;
-}
-
-// Write password to the #password input
-function updatePassword(){
-  const passwordLength = 8;
-  const password = generatePassword(passwordLength);
-   document.getElementById('password').textContent = password;
+  
+// For every loop pick a random character from the choice array
 
 }
-
-// Add event listener to generate button
-document.getElementById('password').addEventListener("click", updatePassword);
+console.log(password);
